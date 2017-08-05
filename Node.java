85bb0b76c738c3f49144b26c931200f76b769489
kavinkumar06 <kavinm09@gmail.com>
2017-08-05 11:25:11 +0530

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,29 @@
+# Node
+import java.io.*;
+import java.util.*;
+public class Node{  
+  
+  public static void main(String[] args)throws IOException 
+  {
+    
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    
+    
+    String str=br.readLine();
+    String revstring="";
+    
+    for(int i=str.length()-1;i>=0;--i){
+      revstring +=str.charAt(i);
+    }
+    
+    System.out.println(revstring);
+    
+    if(revstring.equalsIgnoreCase(str)){
+      System.out.println("The string is Palindrome");
+    }
+    else{
+      System.out.println("Not Palindrome");
+    }
+    
+  }
+}
